844ddb5573227f7227f146f75f6189f9986e5b86
Quadrum <38959426+Quadrum1@users.noreply.github.com>
2022-07-19 16:59:47 +0200

    Quick Fix

--- client/discord/library/common.js
+++ client/discord/library/common.js
@@ -4,7 +4,13 @@ module.exports = client => {
             const sectorEmbed = client.extends.embed();
             sectorEmbed.title = title;
             sectorEmbed.description = `Cannot display sector until a \`scan\` has been completed.`;
-
+            
+            const btnScan = client.extends.button({
+                id: 'btn_scan',
+                label: 'Scan Sector',
+                style: 'PRIMARY'
+            });
+            
             const buttons = client.extends.row()
                 .addComponents(btnScan);
 
@@ -30,7 +36,25 @@ module.exports = client => {
             sectorEmbed.description = `There is nothing of interest in this sector, just empty space!
 
 You can \`jump\` to another sector or \`scan\` the sector again.`;
+            
+            
+            const btnScan = client.extends.button({
+                id: 'btn_scan',
+                label: 'Scan Sector',
+                style: 'PRIMARY'
+            });
 
+            const btnJump = client.extends.button({
+                id: 'btn_jump',
+                label: 'Jump',
+                style: 'PRIMARY'
+            });
+
+            const btnMap = client.extends.button({
+                id: 'btn_map',
+                label: 'Map',
+                style: 'PRIMARY'
+            });
             const buttons = client.extends.row()
                 .addComponents(btnScan)
                 .addComponents(btnJump)
